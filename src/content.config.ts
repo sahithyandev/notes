59a1db80f638fb8c63303f1763aad2b010e6bba1
Sahithyan Kandathasan <sahithyan2701@gmail.com>
2026-04-18 12:02:07 +0530

    define notes as a common collection

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,22 +2,18 @@ import { defineCollection } from 'astro:content';
 import { glob } from 'astro/loaders';
 import { z } from 'astro/zod';
 
-function createSemesterCollection(directory: string) {
-    return defineCollection({
-        loader: glob({ base: `./docs/${directory}`, pattern: '**/*.{md,mdx}' }),
-        schema: z.object({
-            title: z.string(),
-            slug: z.string(),
-            sidebar: z.object({
-                label: z.string(),
-                order: z.number(),
-            }),
-            prev: z.boolean().optional(),
-            next: z.boolean().optional(),
+const notes = defineCollection({
+    loader: glob({ base: `./docs`, pattern: '**/*.{md,mdx}' }),
+    schema: z.object({
+        title: z.string(),
+        slug: z.string(),
+        sidebar: z.object({
+            label: z.string(),
+            order: z.number(),
         }),
-    });
-}
+        prev: z.boolean().optional(),
+        next: z.boolean().optional(),
+    }),
+});
 
-const s1 = createSemesterCollection("s1");
-
-export const collections = { s1 };
+export const collections = { notes };
