9dcea0dea2db116598d84ed7c6eefb4829824123
Sahithyan Kandathasan <sahithyan2701@gmail.com>
2026-04-18 14:31:24 +0530

    render maths equations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import rehypeKatex from 'rehype-katex';
+import remarkMath from 'remark-math';
 
 // https://astro.build/config
 export default defineConfig({
+  markdown: {
+    rehypePlugins: [rehypeKatex],
+    remarkPlugins: [remarkMath],
+  },
   vite: {
     plugins: [tailwindcss()]
   }
