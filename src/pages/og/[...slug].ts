db5f47cbe005bd76506bc5fc1cf603aff6b472f0
Sahithyan Kandathasan <sahithyan2701@gmail.com>
2026-04-22 18:58:30 +0530

    fix issues in og image route

--- src/pages/og/[...slug].ts
+++ src/pages/og/[...slug].ts
@@ -1,8 +1,9 @@
 import { getCollection } from "astro:content";
 import { OGImageRoute } from "astro-og-canvas";
-import { SITE_TITLE } from "../../config.mjs";
 
-const entries = await getCollection("docs");
+const SITE_TITLE = "Sahithyan's Notes";
+
+const entries = await getCollection("notes");
 
 const pages: Record<
   string,
@@ -20,32 +21,19 @@ for (const entry of entries) {
   pages[_id] = { data };
 }
 
-// biome-ignore lint: Astro check fails when used as "pages.index"
-pages["index"] = {
-  data: {
-    title: "What's this?",
-    editUrl: true,
-    head: [],
-    template: "doc",
-    sidebar: entries[0].data.sidebar,
-    pagefind: true,
-    draft: false,
-  },
-};
-
-export const { getStaticPaths, GET } = OGImageRoute({
+export const {getStaticPaths, GET} = await OGImageRoute({
   pages,
   param: "slug",
   getImageOptions: (_path, page: (typeof pages)[number]) => {
     return {
-      title: page.data.title, // concat(" | ", SITE_TITLE),
+      title: page.data.title,
       description: "On ".concat(SITE_TITLE),
       bgGradient: [[250, 254, 247]],
-      logo: {
-        path: "./public/logo.png",
-        size: [170],
-      },
-      fonts: ["./public/fonts/DMSans_36pt-Regular.ttf"],
+      // logo: {
+      //   path: "./public/logo.png",
+      //   size: [170],
+      // },
+      // fonts: ["./public/fonts/DMSans_36pt-Regular.ttf"],
       font: {
         title: {
           color: [0, 0, 0],
