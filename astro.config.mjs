65c9e92862a1a0e67ac8bda642973926605598ab
Sahithyan Kandathasan <sahithyan2701@gmail.com>
2026-04-24 09:16:02 +0530

    fix note components not displayed correctly

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import rehypeKatex from 'rehype-katex';
 import remarkMath from 'remark-math';
+import mdx from '@astrojs/mdx';
 import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [mdx()],
   markdown: {
     rehypePlugins: [rehypeKatex],
     remarkPlugins: [remarkMath],
