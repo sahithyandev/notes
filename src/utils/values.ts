fa9a56865745185d9492a88f5e9a14035f18e3a3
Sahithyan Kandathasan <sahithyan2701@gmail.com>
2026-05-30 00:12:18 +0530

    add cgpa display on home

--- src/utils/values.ts
+++ src/utils/values.ts
@@ -3,3 +3,5 @@ export const SITE_DOMAIN = "notes.sahithyan.dev";
 export const SITE_HOST_URL = `https://${SITE_DOMAIN}`;
 export const SITE_DESCRIPTION =
   "Sahithyan's engineering notes and study materials";
+
+export const CGPA = 3.82;
